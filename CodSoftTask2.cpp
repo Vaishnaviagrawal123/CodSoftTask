984010b37ec2e51f6726a0ad7c9c5b5a65e1c607
Vaishnaviagrawal123 <mjagrawal37@gmail.com>
2024-07-10 10:55:26 +0530

    Update CodSoftTask2.cpp

--- CodSoftTask2.cpp
+++ CodSoftTask2.cpp
@@ -1,15 +1,16 @@
+// CodSoft Task 2!!
 #include <iostream>
 
 using namespace std;
 
 int main() {
-    double num1, num2;
+    double number1, number2;
     char op;
     
-    cout << "Enter the value of num1: ";
+    cout << "Enter the value of number1: ";
     cin >> num1;
     
-    cout << "Enter the value of num2: ";
+    cout << "Enter the value of number2: ";
     cin >> num2;
     
     cout << "Enter the operator (+ - * /): ";
@@ -17,23 +18,30 @@ int main() {
     
     switch (op) {
         case '+':
-            cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+            cout << number1 << " + " << number2 << " = " << number1 + number2 << endl;
+            // peforming addition operation
             break;
         case '-':
-            cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+            cout << number1 << " - " << number2 << " = " << number1 - number2 << endl;
+            // peforming substraction operation
             break;
         case '*':
-            cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+            cout << number1 << " * " << number2 << " = " << number1 * number2 << endl;
+            // pefroming multiplication operation
             break;
         case '/':
-            if (num2 == 0.0) {
+            if (number2 == 0.0) {
+                // condition for 0
                 cout << "Division by zero situation" << endl;
+                
+                
             } else {
-                cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+                cout << number1 << " / " << number2 << " = " << number1 / number2 << endl;
+                // peforming division operation 
             }
             break;
         default:
-            cout << "Invalid operator!" << endl;
+            cout << "Invalid!" << endl;
             break;
     }
 
